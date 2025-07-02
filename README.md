ec607a21437ff8ea8aa2b182692611da1312d26d
bece21-mkenani <mphatsokenani0@gmail.com>
2025-07-02 11:56:49 +0000

    leveled to 94%

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # My Portfolio
-My Portfolio Project with Andela
+My Portfolio Project
 
-> Documentation [Here - api.kibongo.com](https://api.kibongo.com/)
-
-> Live website on [github pages](https://kibongos40.github.io/my-brand) or [kibongo.com](https://kibongo.com)
-
-> Live API [Here - kibongo.com/api/v1](https://kibongo.com/api/v1)
---
+++ email.js
@@ -0,0 +1,15 @@
+function sendMessage() {
+    let params ={
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+
+    }
+      if (!params.message||!params.email||!params.name){
+        alert("please complete all fields before sending!!")
+    }
+    else{
+       emailjs.send("service_b1hvdip","template_vkte5lc",params).then(alert("your message has been received!!"))
+    }
+   
+}
